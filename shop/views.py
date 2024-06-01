415e8fce4964c6e71eaa0853a0f8873b3a50983e
Matías Senia <matiassenia9@gmail.com>
2024-06-01 13:38:10 -0300

    Add carrito

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
 from .models import Remera, Carrito, ElementoCarrito
 
 
